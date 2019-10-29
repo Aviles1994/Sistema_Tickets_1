91f05752eed0f397556635abdfcb1c8b636dd411
Aviles1994 <gaviles011@accitesz.com>
2019-10-29 13:49:36 -0600

    Proyecto nuevo, necesito explicarlo

---
+++ Sistema_Tickets/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_Tickets.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login() {
+            return View();
+        }
+
+        public ActionResult Enter(string Nombre_Usuario_Login, string contraseña)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error"+ex.Message);
+            }
+
+        }
+    }
+}
